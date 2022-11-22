71ded77ae3c545544ef2455ca481a19e9b9b542b
SlavaLatokhin <st091713@student.spbu.ru>
2022-11-22 22:43:37 +0300

    HW10

--- project/cfpq.py
+++ project/cfpq.py
@@ -2,6 +2,7 @@ from typing import Set
 
 from pyformlang.cfg import CFG
 from networkx import MultiDiGraph
+from scipy.sparse import dok_matrix
 
 from project.cfg import to_weakened_normal_form
 
@@ -91,3 +92,64 @@ def hellings(graph: MultiDiGraph, cfg: CFG):
                 r_changes.add(triple)
         r |= r_changes
     return r
+
+
+def cfpg_by_matrix(
+    graph: MultiDiGraph,
+    cfg: CFG,
+    start_nodes: Set[int] = None,
+    final_nodes: Set[int] = None,
+):
+    if start_nodes is None:
+        start_nodes = graph.nodes
+    if final_nodes is None:
+        final_nodes = graph.nodes
+    start_symbol = cfg.start_symbol.value
+
+    result = {
+        (x, y)
+        for (label, x, y) in eval_matrix(graph, cfg)
+        if x in start_nodes and y in final_nodes and label == start_symbol
+    }
+    return result
+
+
+def eval_matrix(graph: MultiDiGraph, cfg: CFG):
+    n = graph.number_of_nodes()
+
+    wcnf = to_weakened_normal_form(cfg)
+    eps_nonterm = {p.head.value for p in wcnf.productions if not p.body}
+    terms_prod = {p for p in wcnf.productions if len(p.body) == 1}
+    nonterm_prod = {p for p in wcnf.productions if len(p.body) == 2}
+
+    matrices = {
+        nonterm.value: dok_matrix((n, n), dtype=bool) for nonterm in wcnf.variables
+    }
+
+    for i, j, label in graph.edges(data="label"):
+        for nonterm in (
+            tp.head.value for tp in terms_prod if tp.body[0].value == label
+        ):
+            matrices[nonterm][i, j] = True
+
+    for i in range(n):
+        for nonterm in eps_nonterm:
+            matrices[nonterm][i, i] = True
+
+    while True:
+        changed = False
+        for nonterm in nonterm_prod:
+            nnz = matrices[nonterm.head.value].count_nonzero()
+            matrices[nonterm.head.value] += (
+                matrices[nonterm.body[0].value] @ matrices[nonterm.body[1].value]
+            )
+            if matrices[nonterm.head.value].count_nonzero() != nnz:
+                changed = True
+        if not changed:
+            break
+
+    return {
+        (nonterm, i, j)
+        for nonterm, matrix in matrices.items()
+        for i, j in zip(*matrix.nonzero())
+    }
