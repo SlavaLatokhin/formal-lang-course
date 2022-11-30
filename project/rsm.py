c42cd8bb70ec49f26a488a4bcfce49eb3cc6d9fa
SlavaLatokhin <st091713@student.spbu.ru>
2022-12-01 01:01:17 +0300

    HW11

--- project/rsm.py
+++ project/rsm.py
@@ -1,6 +1,6 @@
 from typing import NamedTuple, Dict
 
-from pyformlang.finite_automaton import EpsilonNFA
+from pyformlang.finite_automaton import EpsilonNFA, State
 from pyformlang.cfg import Variable
 
 from project.ecfg import ECFG
@@ -13,10 +13,25 @@ class RecursiveStateMachine(NamedTuple):
     @classmethod
     def from_ecfg(cls, ecfg: ECFG):
         boxes: Dict[Variable, EpsilonNFA] = dict()
-        for var, regex in ecfg.productions.items():
-            boxes[var] = regex.to_epsilon_nfa()
+        for v, regex in ecfg.productions.items():
+            boxes[v] = regex.to_epsilon_nfa()
         return cls(ecfg.start_symbol, boxes)
 
     def minimize(self):
         boxes = {v: enfa.minimize() for v, enfa in self.boxes.items()}
         return RecursiveStateMachine(self.start_symbol, boxes)
+
+    def to_nfa(self):
+        nfa = EpsilonNFA()
+        for v, enfa in self.boxes.items():
+            for state in enfa.start_states:
+                nfa.add_start_state(State((v, state.value)))
+            for state in enfa.final_states:
+                nfa.add_final_state(State((v, state.value)))
+            for (start, symbol, finish) in enfa:
+                nfa.add_transition(
+                    State((v, start)),
+                    symbol,
+                    State((v, finish)),
+                )
+        return nfa
